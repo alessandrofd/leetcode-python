f071167f82fd4f470f1b14c384ea6eaa1487a1af
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-06 23:41:49 -0300

    864. Shortest path to get all keys (1st rev)

--- 864a-shortest-path-to-get-all-keys.py
+++ 864a-shortest-path-to-get-all-keys.py
@@ -43,6 +43,47 @@ from collections import deque, defaultdict
 
 class Solution:
     def shortestPathAllKeys(self, grid: List[str]) -> int:
+        def to_bitmap(char):
+            return 1 << ((ord(char) & 31) - 1)
+
+        def islocked(char, keys):
+            return char in "ABCDEF" and not to_bitmap(char) & keys
+
+        rows, cols = len(grid), len(grid[0])
+
+        queue = deque()
+        count_keys = 0
+        for row, col in product(range(rows), range(cols)):
+            if grid[row][col] == "@":
+                queue.append((row, col, 0, 0))
+            elif grid[row][col] in "abcdef":
+                count_keys += 1
+
+        all_keys = 2**count_keys - 1
+        visited = defaultdict(set)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+        while queue:
+            row, col, keys, distance = queue.popleft()
+            if (
+                not 0 <= row < rows
+                or not 0 <= col < cols
+                or (row, col) in visited[keys]
+                or grid[row][col] == "#"
+                or islocked(grid[row][col], keys)
+            ):
+                continue
+
+            visited[keys].add((row, col))
+
+            if grid[row][col] in "abcedf":
+                keys |= to_bitmap(grid[row][col])
+                if keys == all_keys:
+                    return distance
+
+            for move_row, move_col in moves:
+                queue.append((row + move_row, col + move_col, keys, distance + 1))
+
         return -1
 
 
