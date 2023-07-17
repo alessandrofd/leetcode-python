1594609f4ad1b4e9e45d9790ff34375bf85458bc
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-16 22:41:22 -0300

    1575a. Count all possible routes

--- 1575a-count-all-possible-routes.py
+++ 1575a-count-all-possible-routes.py
@@ -33,17 +33,79 @@ class Solution:
     def countRoutes_dp_top_down(
         self, locations: List[int], start: int, finish: int, fuel: int
     ) -> int:
-        return 0
+        @cache
+        def dfs(origin, fuel_left):
+            if fuel_left < 0:
+                return 0
+
+            routes = 1 if origin == finish else 0
+
+            for destination in range(len(locations)):
+                if origin == destination:
+                    continue
+
+                fuel_cost = abs(locations[origin] - locations[destination])
+                routes += dfs(destination, fuel_left - fuel_cost)
+
+            return routes
+
+        return dfs(start, fuel) % 1_000_000_007
 
     def countRoutes_dp_bottom_up(
         self, locations: List[int], start: int, finish: int, fuel: int
     ) -> int:
-        return 0
+        n = len(locations)
+        dp = [[0] * (fuel + 1) for _ in range(n)]
+        dp[finish] = [1] * (fuel + 1)
+
+        for fuel_left in range(fuel + 1):
+            for origin in range(n):
+                for destination in range(n):
+                    if origin == destination:
+                        continue
+
+                    fuel_cost = abs(locations[origin] - locations[destination])
+                    if fuel_cost > fuel_left:
+                        continue
+
+                    dp[origin][fuel_left] += dp[destination][fuel_left - fuel_cost]
+
+        return dp[start][fuel] % 1_000_000_007
 
     def countRoutes_dp_top_down_optimized(
         self, locations: List[int], start: int, finish: int, fuel: int
     ) -> int:
-        return 0
+        n = len(locations)
+        fuel_costs = defaultdict(list)
+        for origin in range(n):
+            for destination in range(origin + 1, n):
+                fuel_cost = abs(locations[origin] - locations[destination])
+
+                if fuel_cost > fuel:
+                    continue
+
+                fuel_costs[origin].append((fuel_cost, destination))
+                fuel_costs[destination].append((fuel_cost, origin))
+
+        for fuel_costs_city in fuel_costs.values():
+            fuel_costs_city.sort()
+
+        @cache
+        def dfs(origin, fuel_left):
+            routes = 1 if origin == finish else 0
+
+            for fuel_cost, destination in fuel_costs[origin]:
+                # Como o combustível necessário para a viagem para as próximas
+                # cidades está ordenado, não precisamos percorrer todas as
+                # possibilidades
+                if fuel_cost > fuel_left:
+                    break
+
+                routes += dfs(destination, fuel_left - fuel_cost)
+
+            return routes
+
+        return dfs(start, fuel) % 1_000_000_007
 
 
 def test_solution():
