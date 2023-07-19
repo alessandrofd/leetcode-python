762306a2bdc8807422dd20782605458a742cf434
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-18 22:46:53 -0300

    137a. Single number II

--- 137a-single-number-ii.py
+++ 137a-single-number-ii.py
@@ -23,7 +23,14 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return -1
+        ones = 0
+        twos = 0
+
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+
+        return ones
 
 
 def test_solution():
