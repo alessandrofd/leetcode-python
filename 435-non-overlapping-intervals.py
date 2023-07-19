7b2c8bd3661b825e297a51e32ecf48aae0b1392d
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-19 07:24:31 -0300

    435. Non overlapping intervals

---
+++ 435-non-overlapping-intervals.py
@@ -0,0 +1,49 @@
+"""
+ * Given an array of intervals intervals where intervals[i] = [starti, endi],
+ * return the minimum number of intervals you need to remove to make the rest of
+ * the intervals non-overlapping.
+ *
+ * Constraints:
+ *    1 <= intervals.length <= 10^5
+ *    intervals[i].length == 2
+ *    -5 * 10^4 <= starti < endi <= 5 * 10^4
+"""
+
+from typing import List
+
+# Greedy
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        n = len(intervals)
+        intervals.sort(key=lambda i: i[1])
+
+        end = intervals[0][1]
+        erases = 0
+        for next_start, next_end in intervals[1:]:
+            if next_start >= end:
+                end = next_end
+            else:
+                erases += 1
+
+        return erases
+
+
+def test_solution():
+    """test"""
+
+    funcs = [
+        Solution().eraseOverlapIntervals,
+    ]
+
+    # fmt: off
+    data = [
+        ([[1,2],[2,3],[3,4],[1,3]], 1),
+        ([[1,2],[1,2],[1,2]], 2),
+        ([[1,2],[2,3]], 0),
+    ]
+    # fmt: on
+    for intervals, expected in data:
+        for func in funcs:
+            assert func(intervals) == expected
