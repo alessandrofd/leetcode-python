e9bc6fb97266bb370679a4db31db5d6bb6b8bced
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-26 11:05:43 -0300

    1870. Minimum speed to arrive on time

---
+++ 1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,80 @@
+"""
+You are given a floating-point number hour, representing the amount of time
+you have to reach the office. To commute to the office, you must take
+n trains in sequential order. You are also given an integer array dist of
+length n, where dist[i] describes the distance (in kilometers) of the ith
+train ride.
+
+Each train can only depart at an integer hour, so you may need to wait in
+between each train ride.
+
+    For example, if the 1st train ride takes 1.5 hours, you must wait for
+    an additional 0.5 hours before you can depart on the 2nd train ride at
+    the 2 hour mark.
+
+Return the minimum positive integer speed (in kilometers per hour) that all
+the trains must travel at for you to reach the office on time, or -1 if it is
+impossible to be on time.
+
+Tests are generated such that the answer will not exceed 10^7 and hour will
+have at most two digits after the decimal point.
+
+Constraints:
+    n == dist.length
+    1 <= n <= 10^5
+    1 <= dist[i] <= 10^5
+    1 <= hour <= 10^9
+    There will be at most two digits after the decimal point in hour.
+"""
+from typing import List
+from math import floor, ceil
+
+
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        n = len(dist)
+        if hour <= n - 1:
+            return -1
+
+        def time(speed):
+            t = sum(ceil(d / speed) for d in dist[:-1])
+            t += dist[-1] / speed
+            return t
+
+        sum_dist = sum(dist)
+        lo = max(floor(sum_dist / hour), 1)
+        hi = ceil(sum_dist / (hour - (n - 1)))
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if time(mid) > hour:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return lo
+
+
+def test_solution():
+    """test"""
+
+    funcs = [
+        Solution().minSpeedOnTime,
+    ]
+
+    # fmt: off
+    data = [
+        ([1, 3, 2], 6, 1),
+        ([1, 3, 2], 2.7, 3),
+        ([1, 3, 2], 1.9, -1),
+        ([1, 1, 100000], 2.01, 10000000),
+        ([5, 3, 4, 6, 2, 2, 7], 10.92, 4),
+        ([1, 1], 1.0, -1),
+        ([69], 4.6, 15),
+        ([6,10,5,1,8,9,2], 34.0, 2),
+        ([2, 1, 5, 4, 4, 3, 2, 9, 2, 10], 75.12, 1),
+    ]
+    # fmt: on
+    for dist, hour, expected in data:
+        for func in funcs:
+            assert func(dist, hour) == expected
