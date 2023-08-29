9fbc601e91252c7c14e63ccf582256e30abba214
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-08-28 23:25:35 -0300

    2483. Minimum penalty for a shop

---
+++ 2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,65 @@
+"""
+You are given the customer visit log of a shop represented by a 0-indexed
+string customers consisting only of characters 'N' and 'Y':
+
+    if the ith character is 'Y', it means that customers come at the ith hour
+
+    whereas 'N' indicates that no customers come at the ith hour.
+
+If the shop closes at the jth hour (0 <= j <= n), the penalty is calculated
+as follows:
+
+    For every hour when the shop is open and no customers come, the penalty
+    increases by 1.
+
+    For every hour when the shop is closed and customers come, the penalty
+    increases by 1.
+
+Return the earliest hour at which the shop must be closed to incur a minimum
+penalty.
+
+Note that if a shop closes at the jth hour, it means the shop is closed at
+the hour j.
+
+Constraints:
+    1 <= customers.length <= 10^5
+    customers consists only of characters 'Y' and 'N'.
+"""
+
+
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+
+        best_time = 0
+        penalty = 0
+        min_penalty = 0
+        for i, customer in enumerate(customers):
+            penalty += -1 if customer == "Y" else 1
+            if penalty < min_penalty:
+                min_penalty = penalty
+                best_time = i + 1
+
+        return best_time
+
+
+def test_solution():
+    """test"""
+
+    funcs = [Solution().bestClosingTime]
+
+    # fmt: off
+    data = [
+        ['YYNY', 2],
+        ['NNNNN', 0],
+        ['YYYY', 4],
+    ]
+    # fmt: on
+
+    for customers, expected in data:
+        for func in funcs:
+            assert func(customers) == expected
+
+
+if __name__ == "__main__":
+    pass
