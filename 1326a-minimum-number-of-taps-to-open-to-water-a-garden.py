3b87a243908dc208173b23730175ba81e39612d9
Alessandro Dantas <alessandrofd@yahoo.com.br>
2023-08-31 18:00:58 -0300

    1326. Minimum number of taps to open to water a garden

--- 1326a-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326a-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -22,38 +22,10 @@ from typing import List
 
 class Solution:
     def minTaps_dp(self, n: int, ranges: List[int]) -> int:
-        dp = [n + 2] * (n + 1)
-        dp[0] = 0
-
-        for i, range_tap in enumerate(ranges):
-            start = max(0, i - range_tap)
-            end = min(n, i + range_tap)
-            for j in range(start, end + 1):
-                dp[end] = min(dp[end], dp[j] + 1)
-
-        return -1 if dp[n] == n + 2 else dp[n]
+        return -1
 
     def minTaps_greedy(self, n: int, ranges: List[int]) -> int:
-        max_reach = [0] * (n + 1)
-        for i, range_tap in enumerate(ranges):
-            start = max(0, i - range_tap)
-            end = min(n, i + range_tap)
-            max_reach[start] = max(max_reach[start], end)
-
-        taps = 0
-        curr_end = 0
-        next_end = 0
-        for i, reach in enumerate(max_reach):
-            if i > next_end:
-                return -1
-
-            if i > curr_end:
-                taps += 1
-                curr_end = next_end
-
-            next_end = max(next_end, reach)
-
-        return taps
+        return -1
 
 
 def test_solution():
