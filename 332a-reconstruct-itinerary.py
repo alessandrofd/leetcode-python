6b0197d57cdbf9f9909756d66e543238976dc9c8
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-09-28 22:12:18 -0300

    332a. Reconstruct itinerary

--- 332a-reconstruct-itinerary.py
+++ 332a-reconstruct-itinerary.py
@@ -23,13 +23,28 @@
  *    fromi != toi
 """
 
-from typing import List
 from collections import defaultdict
 
 
 class Solution:
-    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        pass
+    def findItinerary(self, tickets: list[list[str]]) -> list[str]:
+        flights = defaultdict(list)
+        for departure, destination in tickets:
+            flights[departure].append(destination)
+
+        for destinations in flights.values():
+            destinations.sort(reverse=True)
+
+        itinerary = []
+
+        def visit(airport: str) -> None:
+            while flights[airport]:
+                visit(flights[airport].pop())
+
+            itinerary.append(airport)
+
+        visit("JFK")
+        return itinerary[::-1]
 
 
 def test_solution():
@@ -59,6 +74,39 @@ def test_solution():
                 ['ATL', 'SFO'],
             ],
             ['JFK', 'ATL', 'JFK', 'SFO', 'ATL', 'SFO'],
+        ],
+        [
+            [
+                ['JFK', 'KUL'],
+                ['JFK', 'NRT'],
+                ['NRT', 'JFK'],
+            ],
+            ['JFK', 'NRT', 'JFK', 'KUL'],
+        ],
+        [
+            [
+                ['EZE', 'TIA'],
+                ['EZE', 'HBA'],
+                ['AXA', 'TIA'],
+                ['JFK', 'AXA'],
+                ['ANU', 'JFK'],
+                ['ADL', 'ANU'],
+                ['TIA', 'AUA'],
+                ['ANU', 'AUA'],
+                ['ADL', 'EZE'],
+                ['ADL', 'EZE'],
+                ['EZE', 'ADL'],
+                ['AXA', 'EZE'],
+                ['AUA', 'AXA'],
+                ['JFK', 'AXA'],
+                ['AXA', 'AUA'],
+                ['AUA', 'ADL'],
+                ['ANU', 'EZE'],
+                ['TIA', 'ADL'],
+                ['EZE', 'ANU'],
+                ['AUA', 'ANU'],
+            ],
+            [ 'JFK', 'AXA', 'AUA', 'ADL', 'ANU', 'AUA', 'ANU', 'EZE', 'ADL', 'EZE', 'ANU', 'JFK', 'AXA', 'EZE', 'TIA', 'AUA', 'AXA', 'TIA', 'ADL', 'EZE', 'HBA', ],
         ],
     ]
     # fmt: on
