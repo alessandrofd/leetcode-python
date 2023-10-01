94b1e410958f9011866a5c4430037b56f8bf439a
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-09-30 22:03:05 -0300

    557a. Reverse words in a string III

---
+++ 557a-reverse-words-in-a-string-iii.py
@@ -0,0 +1,34 @@
+"""
+Given a string s, reverse the order of characters in each word within a
+sentence while still preserving whitespace and initial word order.
+ *
+Constraints:
+    1 <= s.length <= 5 * 10^4
+    s contains printable ASCII characters.
+    s does not contain any leading or trailing spaces.
+    There is at least one word in s.
+    All the words in s are separated by a single space
+"""
+
+
+class Solution:
+    def reverseWords(self, sentence: str) -> str:
+        return " ".join([word[::-1] for word in sentence.split(" ")])
+
+
+def test_solution():
+    """test"""
+
+    funcs = [
+        Solution().reverseWords,
+    ]
+
+    # fmt: off
+    data = [
+        ["Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc"],
+        ['God Ding', 'doG gniD'],
+    ]
+    # fmt: on
+    for sentence, expected in data:
+        for func in funcs:
+            assert func(sentence) == expected
