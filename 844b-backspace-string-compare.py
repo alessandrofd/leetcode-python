90e4801a7b8d610412e272a6a3f4049550ce5f23
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-10-18 22:51:45 -0300

    844. Backspace string compare

---
+++ 844b-backspace-string-compare.py
@@ -0,0 +1,34 @@
+"""
+Given two strings s and t, return true if they are equal when both are typed
+into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Constraints:
+    1 <= s.length, t.length <= 200
+    s and t only contain lowercase letters and '#' characters.
+"""
+
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        return False
+
+
+def test_solution():
+    """test"""
+
+    funcs = [
+        Solution().backspaceCompare,
+    ]
+
+    # fmt: off
+    data = [
+        ['ab#c', 'ad#c', True],
+        ['ab##', 'c#d#', True],
+        ['a#c', 'b', False],
+    ]
+    # fmt: on
+    for func in funcs:
+        for s, t, expected in data:
+            assert func(s, t) == expected
